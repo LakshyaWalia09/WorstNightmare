a3c74d6196d463b6226233e08ddaadee67104a3d
LakshyaWalia09 <78919409+LakshyaWalia09@users.noreply.github.com>
2022-06-19 21:27:30 +0530

    Add files via upload

---
+++ timpass.py
@@ -0,0 +1,23 @@
+age = input("what is your age? Ill double it for no reason :D ")
+agemult = int(age) * 2
+print("Twice your age is ")
+print(agemult)
+
+gender = input("what is your gender ")
+print("Pride! you are")
+print(gender)
+
+crush = input("what is your crushs' name? ")
+print("LOL i knew " + gender + " Is your crush fr")
+print("her phone number: 7733774499 Thank me later :)")
+
+temp = input("Tell me whether in celcius ill convert it to farehiet even if u can use calculator :) ")
+tempfarenhiet = int(temp) + 273.15
+print("Temperature in Kalvin : " + str(tempfarenhiet) + " K")
+
+number1 = input("Pehla number bolo ill add for you :) ")
+number2 = input("doosra bhi bolde ab? aur calculator isi kam ke liye hota hai :D ")
+resultadd = int(number1) + int(number2)
+print("Tumhare sawaal ka jawaab hai : " + str(number1) + " + " + str(number2) + " equals to " + str(resultadd) + " Sahi hai na?")
+print("powered by sona chandi chawanprash :D ")
+print("Sasta copyright : Lakshya Walia . com !")
