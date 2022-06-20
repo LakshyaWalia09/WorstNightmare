e519debe912b2106cb4af5be6e03206fb84dfc73
LakshyaWalia09 <78919409+LakshyaWalia09@users.noreply.github.com>
2022-06-20 15:02:59 +0530

    Update timpass.py

--- timpass.py
+++ timpass.py
@@ -1,23 +1,37 @@
-age = input("what is your age? Ill double it for no reason :D ")
+name = input("Hi tumhara shubh nam kya hai?")
+input("Hi " + name + " welcome, press Enter to continue! ")
+print(" ")
+
+age = input(name + ", what is your age? Ill double it for no reason :D ")
 agemult = int(age) * 2
 print("Twice your age is ")
 print(agemult)
+print(" ")
 
-gender = input("what is your gender ")
-print("Pride! you are ")
-print(gender)
+gender = input("What is your gender? ")
+print("Pride! you are " + gender )
+print(" ")
 
-crush = input("what is your crushs' name? ")
-print("LOL i knew " + gender + " Is your crush fr ")
-print("her phone number: 7693774499 Thank me later :) ")
+
+crush = input("What is your crushs' name? ")
+print("LOL i knew " + str(crush) + " Is your crush fr ")
+print("her phone number: 7733774499 Thank me later :) ")
+print(" ")
 
 temp = input("Tell me whether in celcius ill convert it to farehiet even if u can use calculator :) ")
 tempfarenhiet = int(temp) + 273.15
 print("Temperature in Kalvin : " + str(tempfarenhiet) + " K")
+print(" ")
 
 number1 = input("Pehla number bolo ill add for you :) ")
 number2 = input("doosra bhi bolde ab? aur calculator isi kam ke liye hota hai :D ")
 resultadd = int(number1) + int(number2)
 print("Tumhare sawaal ka jawaab hai : " + str(number1) + " + " + str(number2) + " equals to " + str(resultadd) + " Sahi hai na?")
+print(" ")
 print("powered by sona chandi chawanprash :D ")
-print("Sasta copyright : Lakshya Walia . com !3")
+print("Sasta copyright : Lakshya Walia . com !")
+
+#Khatam karte hain
+print("")
+input('Press ENTER to exit kyunki isse jada main nhi kar raha :)')
+
